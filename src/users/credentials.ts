1c4e8e9d55304bba838ae4587ab67a02fd8488fc
Onkur00 <146575884+Onkur00@users.noreply.github.com>
2026-06-11 22:50:09 +0600

    Update credentials.ts

--- src/users/credentials.ts
+++ src/users/credentials.ts
@@ -1,24 +1,87 @@
 export interface UserCredential {
   username: string;
-  password?: string;
+  password: string;
   deviceLimit: number;
   displayName: string;
 }
 
 export const userCredentials: UserCredential[] = [
 
-  
-  
+
+
+
+
   {
-    username: "sb938",
-    password: "0104",
+    username: "",
+    password: "0000",
     deviceLimit: 3,
-    displayName: "sb938"
+    displayName: "SB User"
   },
   {
-    username: "sb245",
+    username: "",
+    password: "0000",
+    deviceLimit: 3,
+    displayName: "SB User"
+  },
+  {
+    username: "",
+    password: "0000",
+    deviceLimit: 3,
+    displayName: "SB User"
+  },
+  {
+    username: "",
+    password: "0000",
+    deviceLimit: 3,
+    displayName: "SB User"
+  },
+  {
+    username: "sb715",
+    password: "5317",
+    deviceLimit: 3,
+    displayName: "SB User"
+  },
+  {
+    username: "sb0",
+    password: "0000",
+    deviceLimit: 3,
+    displayName: "SB User"
+  },
+  {
+    username: "sb539",
     password: "1234",
     deviceLimit: 3,
-    displayName: "sb245"
+    displayName: "SB User"
   },
+  {
+    username: "sb1001",
+    password: "7070",
+    deviceLimit: 2,
+    displayName: ""
+    displayName: "SB User"
+  },
+  {
+    username: "sb499",
+    password: "5585",
+    deviceLimit: 5,
+    displayName: "SB User"
+  },
+  {
+    username: "guest",
+    password: "5555",
+    deviceLimit: 1,
+    displayName: "SB User"
+  },
+  {
+    username: "sb655",
+    password: "5270",
+    deviceLimit: 4,
+    displayName: "SB User"
+  },
+  {
+    username: "sb728",
+    password: "4826",
+    deviceLimit: 3,
+    displayName: "SB User"
+  }
 ];
